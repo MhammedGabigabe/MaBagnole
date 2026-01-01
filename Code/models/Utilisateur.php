36047ed7667882064a7a2ba1d6b1e8456b81c11d
Gabigabe <mhammed.gabigabe@gmail.com>
2026-01-02 00:48:16 +0100

    refactor(models): rendre la classe Utilisateur abstraite

--- Code/models/Utilisateur.php
+++ Code/models/Utilisateur.php
@@ -1,14 +1,14 @@
 <?php
 require_once "BaseModel.php";
 
-class Utilisateur extends BaseModel{
+abstract class Utilisateur extends BaseModel{
     protected $idUtilisateur;
     protected $nom;
     protected $email;
     protected $mdp;
     protected $role;
 
-    
+
 
 }
 
