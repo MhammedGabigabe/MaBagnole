298c2badb40889db977da704d533bbf46e34e294
Gabigabe <mhammed.gabigabe@gmail.com>
2026-01-02 00:59:24 +0100

    feat(models): implémenter les méthodes magiques __get et __set dans Utilisateur

--- Code/models/Utilisateur.php
+++ Code/models/Utilisateur.php
@@ -8,7 +8,21 @@ abstract class Utilisateur extends BaseModel{
     protected $mdp;
     protected $role;
 
+    public function __set($p, $v){
+        if(property_exists($this, $p)){
+            $this->$p = $v;
+        }else{
+            throw new Exception("la propriété '$p' n'existe pas dans la classe" .get_class($this));
+        }
+    }
 
+    public function __get($p){
+        if(property_exists($this,$p)){
+            return $this->$p;
+        }else{
+            throw new Exception("la propriété '$p' n'existe pas dans la classe" .get_class($this));
+        }
+    }
 
 }
 
