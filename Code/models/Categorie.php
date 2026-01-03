10f765e5e1dd93ccbb8a803a414b2fbe813e75e2
Gabigabe <mhammed.gabigabe@gmail.com>
2026-01-03 14:00:10 +0100

    feat(models): creer la classe Categorie

---
+++ Code/models/Categorie.php
@@ -0,0 +1,29 @@
+<?php
+require_once "BaseModel.php";
+
+class Categorie extends BaseModel {
+    private $idCategorie;
+    private $nom;
+    private $description ;
+
+    public function getId(){
+        return $this->idCategorie;
+    }
+
+    public function setNom($valeur){
+        $this->nom = $valeur;
+    }
+
+    public function getNom(){
+        return $this->nom;
+    }
+
+    public function setDescription($valeur){
+        $this->description = $valeur;
+    }
+
+    public function getDescription(){
+        return $this->description;
+    }
+}
+?>
